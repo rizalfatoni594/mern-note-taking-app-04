251a94c94982519643647849d5012fe9ef6ffc6b
rizal <rizalfatoni594@gmail.com>
2026-08-14 23:18:15 +0700

    Done with CreatePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import NavBar from '../components/NavBar.jsx';
 import RateLimitedUI from '../components/RateLimitedUI.jsx';
-import axios from 'axios';
+import { api } from '../lib/axios.js';
 import toast from 'react-hot-toast';
 import NoteCard from '../components/NoteCard.jsx';
 
@@ -13,7 +13,7 @@ export default function HomePage() {
   useEffect(() => {
     async function fetchNotes() {
       try {
-        const res = await axios.get('http://localhost:5001/api/notes');
+        const res = await api.get('/notes');
         console.log(res.data);
         setNotes(res.data);
         setIsRateLimited(false);
