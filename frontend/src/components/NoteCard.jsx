6b77811fb109e18d62a3d9a766a3423fa5c0d998
rizal <rizalfatoni594@gmail.com>
2026-08-15 15:31:00 +0700

    Done with Delete Functionality.

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -1,8 +1,25 @@
 import { PenSquareIcon, Trash2Icon } from 'lucide-react';
 import { Link } from 'react-router';
 import { formatDate } from '../lib/utils.js';
+import { api } from '../lib/axios.js';
+import toast from 'react-hot-toast';
 
-export default function NoteCard({ note }) {
+export default function NoteCard({ note, setNotes }) {
+  async function handleDelete(e, id) {
+    e.preventDefault();
+
+    if (!window.confirm('Are you sure you want to delete this note?')) return;
+
+    try {
+      await api.delete(`/notes/${id}`);
+      setNotes((prev) => prev.filter((note) => note._id !== id));
+      toast.success('Note deleted successfully.');
+    } catch (error) {
+      console.log('Error in handleDelete.', error);
+      toast.error('Failed to delete the note.');
+    }
+  }
+
   return (
     <Link
       to={`/note/${note._id}`}
@@ -18,7 +35,10 @@ export default function NoteCard({ note }) {
           </span>
           <div className='flex items-center gap-1'>
             <PenSquareIcon className='size-4' />
-            <button className='btn btn-ghost btn-xs text-error'>
+            <button
+              className='btn btn-ghost btn-xs text-error'
+              onClick={(e) => handleDelete(e, note._id)}
+            >
               <Trash2Icon className='size-4' />
             </button>
           </div>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import RateLimitedUI from '../components/RateLimitedUI.jsx';
 import { api } from '../lib/axios.js';
 import toast from 'react-hot-toast';
 import NoteCard from '../components/NoteCard.jsx';
+import NotesNotFound from '../components/NotesNotFound.jsx';
 
 export default function HomePage() {
   const [isRateLimited, setIsRateLimited] = useState(false);
@@ -44,10 +45,11 @@ export default function HomePage() {
           <div className='text-center text-primary py-10'>Loading notes...</div>
         )}
 
+        {notes.length === 0 && !isRateLimited && <NotesNotFound />}
         {notes.length > 0 && !isRateLimited && (
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
             {notes.map((note) => (
-              <NoteCard key={note._id} note={note} />
+              <NoteCard key={note._id} note={note} setNotes={setNotes} />
             ))}
           </div>
         )}
